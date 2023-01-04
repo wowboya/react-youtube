7164087639e696ecc333f2bf250663d1dd16c1d6
10152136 <taesung111.kim@kt.com>
2023-01-04 22:51:22 +0900

    오타 정정

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -3,9 +3,9 @@ import Youtube from "../components/api/youtube";
 import YoutubeClient from "../components/api/youtubeClient";
 
 export const YoutubeApiContext = createContext();
-\
+
 const client = new YoutubeClient();
-const youtube = new Youtube(client); //new Youtube();
+const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
   return (
