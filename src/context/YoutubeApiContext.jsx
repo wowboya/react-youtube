06057ddb70cfd736d6fc305c955b8147460d2838
10152136 <taesung111.kim@kt.com>
2023-01-04 22:49:33 +0900

    realYoutube API import 누락된 부분 추가

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,10 +1,9 @@
 import { createContext, useContext } from "react";
-import FakeYoutubeClient from "../components/api/fakeYoutubeClient";
 import Youtube from "../components/api/youtube";
+import YoutubeClient from "../components/api/youtubeClient";
 
 export const YoutubeApiContext = createContext();
-
-// const client = new FakeYoutubeClient();
+\
 const client = new YoutubeClient();
 const youtube = new Youtube(client); //new Youtube();
 
